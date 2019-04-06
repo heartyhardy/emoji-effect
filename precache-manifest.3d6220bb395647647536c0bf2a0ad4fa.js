da878e0e7e6a75f19c13b6210b5ef9956e1dfe7c
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-04-06 12:07:41 +0530

    Updates

---
+++ precache-manifest.3d6220bb395647647536c0bf2a0ad4fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b689d242cfeb71502192",
+    "url": "/emoji-effect/static/css/main.311ca508.chunk.css"
+  },
+  {
+    "revision": "b689d242cfeb71502192",
+    "url": "/emoji-effect/static/js/main.5ad7f78c.chunk.js"
+  },
+  {
+    "revision": "e3dfd7b47e4d117a5220",
+    "url": "/emoji-effect/static/js/runtime~main.5eeb0627.js"
+  },
+  {
+    "revision": "1ed47eb8a12884a418eb",
+    "url": "/emoji-effect/static/js/2.3db1a4b2.chunk.js"
+  },
+  {
+    "revision": "5db4362dd174819d3e3674d8ba79775d",
+    "url": "/emoji-effect/index.html"
+  }
+];
